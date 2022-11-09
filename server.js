8834c90b35c7f7365db5da7b499bf9bf568ad26c
Joris <je.tieleman@gmail.com>
2022-11-09 13:54:24 +0100

    lesson4 raw node vieuws

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const http = require('http')
+const fs = require('fs');
+const { defaultMaxListeners } = require('events');
 
 const server = http.createServer((req,res) => {
-    console.log('request made')
+    
+    res.setHeader('Content-Type', 'text/html')
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            // Redirect about-me to about
+            res.statusCode = 301;
+            res.setHeader('location', '/about')
+            res.end()
+            break;
+        default:
+            path += '404.html'
+            res.statusCode = 404;
+            break;
+    }
+
+
+    fs.readFile(path, (err, data) => {
+        if (err){
+            throw err
+        }
+        res.end(data)
+    })
 })
 
 server.listen(3000, 'localhost', () => {
     console.log('HEEEEEY')
-})
+}) 
